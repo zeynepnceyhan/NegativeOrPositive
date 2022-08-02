36a33bc0d49d80be6aad0aefacbdc5cb25bbdf08
Zeynep Ceyhan <104215922+zeynepnceyhan@users.noreply.github.com>
2022-08-02 12:17:39 +0300

    is the entered number negative or positive
    internship

---
+++ NegativePositive/NegativePositive/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace methods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            int number1;
+            Console.WriteLine("Please enter a number: ");
+            number1 = Convert.ToInt32(Console.ReadLine());
+
+
+            if (number1 > 0)
+            {
+                Console.WriteLine("The number is a positive number...");
+            }
+            else if (number1 == 0)
+            {
+                Console.WriteLine("The number is not positive or negative. The number is zero...");
+            }
+            else
+            {
+                Console.WriteLine("The number is a negative number...");
+            }
+
+        }
+
+    }
+
+}
